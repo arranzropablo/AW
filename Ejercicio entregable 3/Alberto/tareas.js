43ee7ebfa651a075118177053562fbd0769ae1fc
caminosajalberto <jalbercamino@gmail.com>
2017-10-27 16:43:33 +0200

    Really finished ej3

--- Ejercicio entregable 3/Alberto/tareas.js
+++ Ejercicio entregable 3/Alberto/tareas.js
@@ -10,49 +10,56 @@
 "use strict";
 
 let listaTareas = [
-  { text: "Preparar práctica PDAP", tags: ["pdap", "practica"] },
-  { text: "Mirar fechas congreso", done: true, tags: [] },
-  { text: "Ir al supermercado", tags: ["personal"] },
-  { text: "Mudanza", done: false, tags: ["personal"] },
+    { text: "Preparar práctica PDAP", tags: ["pdap", "practica"] },
+    { text: "Mirar fechas congreso", done: true, tags: [] },
+    { text: "Ir al supermercado", tags: ["personal"] },
+    { text: "Mudanza", done: false, tags: ["personal"] },
 ];
-
-console.log(getToDoTasks(listaTareas));
-console.log(findByTag(listaTareas, "personal"));
 
 /**
  * Devuelve las tareas de la lista de entrada que no hayan sido finalizadas.
  */
 function getToDoTasks(tasks) {
-    let toDo = tasks.filter(n => !n.done);
-    return toDo.map(n => n.text);
+    let toDo = tasks.filter(task => !task.done);
+    return toDo.map(task => task.text);
 }
 
 /**
  * Devuelve las tareas que contengan el tag especificado
  */
 function findByTag(tasks, tag) {
-    return tasks.filter(n => n.tags.indexOf(tag) != -1);
+    return tasks.filter(task => task.tags.indexOf(tag) !== -1);
 }
 
 /**
  * Devuelve las tareas que contengan alguno de los tags especificados
  */
 function findByTags(tasks, tags) {
-  /* Implementar */
+    return tasks.filter(task => task.tags.some(x => tags.indexOf(x) !== -1));
 }
 
 /**
  * Devuelve el número de tareas finalizadas
  */
 function countDone(tasks) {
-  /* Implementar */
+    return tasks.reduce((acum, n) => { if (n.done) { acum++; } return acum }, 0);
 }
 
 /**
  * Construye una tarea a partir de un texto con tags de la forma "@tag"
  */
 function createTask(text) {
-  /* Implementar */
+    let exp = /@[a-z]+/ig;
+
+    let tags = text.match(exp);
+
+    let task = { text: "", tags: [] };
+
+    task.text = text.replace(exp, "").trim();
+    if (tags !== null) {
+        tags.forEach(tag => task.tags.push(tag.replace("@", "")));
+    }
+    return task;
 }
 
 
@@ -61,9 +68,9 @@ function createTask(text) {
   Es necesario para la ejecución de tests
 */
 module.exports = {
-  getToDoTasks: getToDoTasks,
-  findByTag: findByTag,
-  findByTags: findByTags,
-  countDone: countDone,
-  createTask: createTask
+    getToDoTasks: getToDoTasks,
+    findByTag: findByTag,
+    findByTags: findByTags,
+    countDone: countDone,
+    createTask: createTask
 }
