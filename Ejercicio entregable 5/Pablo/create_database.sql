d0d62cdc7aefd8a10e786685140df22eb78774ce
Pablo Arranz Ropero <pablo.arranz.ropero@accenture.com>
2017-11-15 20:36:25 +0100

    Creado ejercicio 5

---
+++ Ejercicio entregable 5/Pablo/create_database.sql
@@ -0,0 +1,3 @@
+create table user (email varchar(100), password varchar(100), img varchar(100), primary key(email));
+create table task (id int(11), user varchar(100), text text, done tinyint(1), primary key(id), foreign key (user) references user(email));
+create table tag (taskId int(11), tag varchar(100), primary key(taskId, tag), foreign key (taskId) references task(id));
